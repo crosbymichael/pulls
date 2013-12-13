f0bc83dc7e7eb3a17f77815b964259742e4e4dc1
Roberto G. Hashioka <roberto.hashioka@docker.com>
2013-12-13 00:08:48 +0000

    - Added the issues flag ``--votes`` that shows the number of issues's votes ``+1``

--- github.go
+++ github.go
@@ -111,12 +111,7 @@ func getRepoPath(pth, org string) string {
 
 func getMaintainersIds(pth string) (*[]string, error) {
 	maintainersFileMap := []string{}
-	file, err := os.Open(pth)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return &maintainersFileMap, err
-		}
-	}
+	file, _ := os.Open(pth)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		strEmail, isEmail, err := getEmailFromLine(scanner.Text())
@@ -223,12 +218,10 @@ func NewMaintainer(client *gh.Client, org, repo string) (*Maintainer, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	err = createMaintainerDirectoriesMap(originPath, "", email, config.UserName, false)
 	if err != nil {
 		return nil, err
 	}
-
 	return &Maintainer{
 		repo:           gh.Repo{Name: repo, UserName: org},
 		client:         client,
@@ -405,7 +398,7 @@ func (m *Maintainer) Checkout(pr *gh.PullRequest) error {
 	return nil
 }
 
-func (m *Maintainer) GetFirstIssue(state, sortBy string) (gh.Issue, error) {
+func (m *Maintainer) GetFirstIssue(state, sortBy string) (*gh.Issue, error) {
 	o := &gh.Options{}
 	o.QueryParams = map[string]string{
 		"state":     state,
@@ -416,10 +409,10 @@ func (m *Maintainer) GetFirstIssue(state, sortBy string) (gh.Issue, error) {
 	}
 	issues, err := m.client.Issues(m.repo, o)
 	if err != nil {
-		return gh.Issue{}, err
+		return nil, err
 	}
 	if len(issues) == 0 {
-		return gh.Issue{}, fmt.Errorf("No matching issues")
+		return nil, fmt.Errorf("No matching issues")
 	}
 	return issues[0], nil
 }
@@ -427,7 +420,7 @@ func (m *Maintainer) GetFirstIssue(state, sortBy string) (gh.Issue, error) {
 // GetIssues queries the GithubAPI for all issues matching the state `state` and the
 // assignee `assignee`.
 // See http://developer.github.com/v3/issues/#list-issues-for-a-repository
-func (m *Maintainer) GetIssues(state, assignee string) ([]gh.Issue, error) {
+func (m *Maintainer) GetIssues(state, assignee string) ([]*gh.Issue, error) {
 	o := &gh.Options{}
 	o.QueryParams = map[string]string{
 		"sort":      "updated",
@@ -442,7 +435,7 @@ func (m *Maintainer) GetIssues(state, assignee string) ([]gh.Issue, error) {
 	}
 	prevSize := -1
 	page := 1
-	all := []gh.Issue{}
+	all := []*gh.Issue{}
 	for len(all) != prevSize {
 		o.QueryParams["page"] = strconv.Itoa(page)
 		if issues, err := m.client.Issues(m.repo, o); err != nil {
