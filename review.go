fbd3dded25df9dfe394a91321adfc12d99698a32
Roberto G. Hashioka <roberto.hashioka@docker.com>
2014-02-14 09:49:57 +0000

    - Updated the reviewers command to look for the current directory tree in order to find the list of maintainers - Improved the reviewers command output

--- review.go
+++ review.go
@@ -1,28 +1,25 @@
 package gordon
 
 import (
-	"bufio"
 	"code.google.com/p/go.codereview/patch"
-	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
 	"path"
-	"path/filepath"
 	"regexp"
 	"strings"
 )
 
 // ReviewPatch reads a git-formatted patch from `src`, and for each file affected by the patch
-// it looks up the hierarchy of MAINTAINERS files in the current directory to
-// determine who should review the patch.
+// it assign its Maintainers based on the current repository tree directories
+// The list of Maintainers are generated when the MaintainerManager object is instantiated.
+//
 // The result is a map where the keys are the paths of files affected by the patch,
 // and the values are the maintainers assigned to review that partiular file.
 //
 // There is no duplicate checks: the same maintainer may be present in multiple entries
 // of the map, or even multiple times in the same entry if the MAINTAINERS file has
 // duplicate lines.
-func ReviewPatch(src io.Reader) (reviewers map[string][]*Maintainer, err error) {
+func ReviewPatch(src io.Reader, maintainersDirMap *map[string][]*Maintainer) (reviewers map[string][]*Maintainer, err error) {
 	reviewers = make(map[string][]*Maintainer)
 	input, err := ioutil.ReadAll(src)
 	if err != nil {
@@ -41,57 +38,20 @@ func ReviewPatch(src io.Reader) (reviewers map[string][]*Maintainer, err error) 
 			if _, exists := reviewers[target]; exists {
 				continue
 			}
-			maintainers, err := getMaintainers(target)
-			if err != nil {
-				return nil, fmt.Errorf("%s: %s\n", target, err)
+			targetDir := "."
+			items := strings.Split(target, "/")
+			for i := 0; i < len(items)-1; i++ {
+				if targetDir == "." {
+					targetDir = items[i]
+				} else {
+					targetDir = path.Join(targetDir, items[i])
+				}
 			}
+			maintainers := (*maintainersDirMap)[targetDir]
 			reviewers[target] = maintainers
 		}
 	}
 	return reviewers, nil
-}
-
-func getMaintainers(target string) (maintainers []*Maintainer, err error) {
-	if _, err := os.Stat(target); err != nil {
-		return nil, err
-	}
-	target, err = filepath.Abs(target)
-	if err != nil {
-		return nil, err
-	}
-	if target == "/" {
-		return []*Maintainer{}, nil
-	}
-	defer func() {
-		if err == nil && (maintainers == nil || len(maintainers) == 0) {
-			maintainers, err = getMaintainers(path.Dir(target))
-		}
-	}()
-	if path.Base(target) == "MAINTAINERS" {
-		tpmf, err := TopMostMaintainerFile(path.Dir(target))
-		if err != nil {
-			return nil, err
-		}
-		if maintainers, exists := tpmf["."]; !exists || len(maintainers) == 0 {
-			return nil, fmt.Errorf("can't find the top-level maintainer to review MAINTAINERS change")
-		} else if lead := maintainers[0]; !lead.Active {
-			return nil, fmt.Errorf("can't review MAINTAINERS change: top-level maintainer %s is inactive", lead.FullName)
-		} else {
-			return []*Maintainer{lead}, nil
-		}
-	}
-	maintainerFile, err := LoadMaintainerFile(path.Dir(target))
-	if os.IsNotExist(err) {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-	if fileMaintainers, exists := maintainerFile[path.Base(target)]; exists {
-		return fileMaintainers, nil
-	} else if dirMaintainers, exists := maintainerFile["."]; exists {
-		return dirMaintainers, nil
-	}
-	return nil, nil
 }
 
 type MaintainerFile map[string][]*Maintainer
@@ -104,26 +64,6 @@ type Maintainer struct {
 	Active   bool
 	Lead     bool
 	Raw      string
-}
-
-func LoadMaintainerFile(dir string) (MaintainerFile, error) {
-	src, err := os.Open(path.Join(dir, "MAINTAINERS"))
-	if err != nil {
-		return nil, err
-	}
-	maintainers := make(MaintainerFile)
-	scanner := bufio.NewScanner(src)
-	for scanner.Scan() {
-		m := parseMaintainer(scanner.Text())
-		if m.Username == "" && m.Email == "" && m.FullName == "" {
-			return nil, fmt.Errorf("Incorrect maintainer format: %s", m.Raw)
-		}
-		if _, exists := maintainers[m.Target]; !exists {
-			maintainers[m.Target] = make([]*Maintainer, 0, 1)
-		}
-		maintainers[m.Target] = append(maintainers[m.Target], m)
-	}
-	return maintainers, nil
 }
 
 func parseMaintainer(line string) *Maintainer {
@@ -144,37 +84,4 @@ func parseMaintainer(line string) *Maintainer {
 		FullName: strings.Trim(match[fullnameIndex], " \t"),
 		Raw:      line,
 	}
-}
-
-// TopMostMaintainerFile moves up the directory tree looking for a MAINTAINERS file,
-// parses the top-most file it finds, and returns its contents.
-// This is used to find the top-level maintainer of a project for certain
-// privileged reviews, such as authorizing changes to a MAINTAINERS file.
-func TopMostMaintainerFile(dir string) (MaintainerFile, error) {
-	if _, err := os.Stat(dir); err != nil {
-		return nil, err
-	}
-	dir, err := filepath.Abs(dir)
-	if err != nil {
-		return nil, err
-	}
-	if dir == "/" {
-		return make(MaintainerFile), nil
-	}
-	parent, err := TopMostMaintainerFile(path.Dir(dir))
-	if err != nil {
-		// Ignore recursive errors which might be caused by
-		// permission errors on parts of the filesystem, etc.
-		parent = make(MaintainerFile)
-	}
-	if len(parent) > 0 {
-		return parent, nil
-	}
-	current, err := LoadMaintainerFile(dir)
-	if os.IsNotExist(err) {
-		return make(MaintainerFile), nil
-	} else if err != nil {
-		return nil, err
-	}
-	return current, nil
 }
